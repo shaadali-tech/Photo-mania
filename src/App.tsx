a632751e598be42063eb8c4f38b9a366a5d7f5df
shaadali-tech <215ics043@gbu.ac.in>
2026-05-10 10:51:48 +0530

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <h1 className="text-6xl font-bold">Welcome to Photoholic</h1>
+    </div>
+  );
+}
+
+export default App;
